8a321a2bd995ddcc3a32ed906bf933b529ee1ae0
xyeaea <rizqiferdiansyah88@gmail.com>
2024-10-26 00:00:57 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Telegram Bot Config
+api_id = "29726374"
+api_hash = "ee797487083e78676bc682c2e78df5fc"
+bot_token = "8058437163:AAF1UQ_VZUSMyTmotuqQyWPEHJ1DihQv2dM"
+
+# Tripay API Key
+tripay_api_key = "TRIPAY_API_KEY"
+
+# MongoDB Config
+mongo_uri = "mongodb+srv://botss:botss@botss.cksa9ke.mongodb.net/?retryWrites=true&w=majority&appName=botss"
+database_name = "database_name"
