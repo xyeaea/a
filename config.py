ff4e4aa408cab788abcf5e177734314e35eabc02
xyeaea <rizqiferdiansyah88@gmail.com>
2024-10-28 15:49:43 +0700

    update config.py

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ api_hash = "ee797487083e78676bc682c2e78df5fc"
 bot_token = "8058437163:AAF1UQ_VZUSMyTmotuqQyWPEHJ1DihQv2dM"
 
 # Tripay API Key
-#tripay_api_key = "DEV-xX59dZok3hhuQSEZCnm4dXxwOTKa8e7jJT3sjXpS"
+tripay_api_key = "DEV-xX59dZok3hhuQSEZCnm4dXxwOTKa8e7jJT3sjXpS"
+tripay_private_key = "lRG4h-dNLSf-x6wK1-Nnc4v-0Skt9" 
+tripay_merchant_code = "T29105"  
 
 # MongoDB Config
 mongo_uri = "mongodb+srv://botss:botss@botss.cksa9ke.mongodb.net/?retryWrites=true&w=majority&appName=botss"
