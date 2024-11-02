99e3a75b3fe81f7efcbbbee1e9984b172664ab42
xyeaea <rizqiferdiansyah88@gmail.com>
2024-11-02 13:19:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 # config.py
 
-# Telegram Bot Config
-api_id = "29726374"
-api_hash = "ee797487083e78676bc682c2e78df5fc"
-bot_token = "8058437163:AAF1UQ_VZUSMyTmotuqQyWPEHJ1DihQv2dM"
+# API ID dan API Hash didapatkan dari my.telegram.org
+api_id = "29726374"  # Ganti dengan API ID Anda
+api_hash = "ee797487083e78676bc682c2e78df5fc"  # Ganti dengan API Hash Anda
 
-# Tripay API Key
-tripay_api_key = "DEV-xX59dZok3hhuQSEZCnm4dXxwOTKa8e7jJT3sjXpS"
-tripay_private_key = "lRG4h-dNLSf-x6wK1-Nnc4v-0Skt9" 
-tripay_merchant_code = "T29105"  
+# Token bot yang didapatkan dari BotFather
+bot_token = "YOUR_BOT_TOKEN"  # Ganti dengan Bot Token Anda
 
-# MongoDB Config
-mongo_uri = "mongodb+srv://botss:botss@botss.cksa9ke.mongodb.net/?retryWrites=true&w=majority&appName=botss"
-db_name = "database_name"
+# Pengaturan tambahan lainnya
+owner_id = 123456789  # Ganti dengan ID Telegram Anda sebagai pemilik bot (opsional)
