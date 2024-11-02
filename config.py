9bb98a6bb0acc41893fe943c37cc52548f4b5feb
xyeaea <rizqiferdiansyah88@gmail.com>
2024-11-02 13:23:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ api_id = "29726374"  # Ganti dengan API ID Anda
 api_hash = "ee797487083e78676bc682c2e78df5fc"  # Ganti dengan API Hash Anda
 
 # Token bot yang didapatkan dari BotFather
-bot_token = "YOUR_BOT_TOKEN"  # Ganti dengan Bot Token Anda
+bot_token = "7207182260:AAFGuEnGvLrZjonyNpcoA3FXB3lho6KkzCQ"  # Ganti dengan Bot Token Anda
 
 # Pengaturan tambahan lainnya
-owner_id = 123456789  # Ganti dengan ID Telegram Anda sebagai pemilik bot (opsional)
+owner_id = 7526061771  # Ganti dengan ID Telegram Anda sebagai pemilik bot (opsional)
